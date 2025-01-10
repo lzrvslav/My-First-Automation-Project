4a9a61c4e3190f463eebca37b26cbede2c7c7c8b
Slav Lazarov <slavlzrv@gmail.com>
2025-01-10 08:01:39 +0200

    Add Lecture5

--- src/Lecture5/Lecture5.java
+++ src/Lecture5/Lecture5.java
@@ -2,132 +2,72 @@ package Lecture5;
 
 public class Lecture5 {
     public static void main(String[] args) {
-        //forLoop();
-        //nestedForLoop();
-        //whileLoop();
-        //doWhileLoop();
-        //forEachLoop();
-        //continueStatement();
-        //breakStatementWhileLoop();
-        breakStatementDoWhileLoop();
+        // forLoop();
+        // nestedForLoop();
+        whileLoop();
     }
-    public static void forLoop(){
-        System.out.println("First For");
-        for (int i =0; i<=8; i++){
-            System.out.println("Number is: " + i);
+
+    public static void forLoop() {
+        System.out.println(" ");
+        System.out.println("This is 1-st 'forLoop': ");
+        // A for loop is a process of repeating a block of code a specified number of times until a given condition is met.
+            // ~this process repeats itself by checking the condition until the condition becomes false, at which point the loop ends.
+        // Statement 1, sets a variable before the loop starts (int i = 0).
+        // Statement 2 defines the condition for the loop to run ('i' must be <= to 10).
+            // ~ if the condition is TRUE, the loop will start over again.
+            // ~ if it is FALSE, the loop will end.
+        // Statement 3 increases a value (i++) each time the code block in the loop has been executed.
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("This is 'i' number: " +i);
         }
-        System.out.println("Second For");
-        for (int i =0; i<=8; ++i){
-            System.out.println("Number is: " + i);
+        System.out.println(" ");
+        System.out.println("This is 2-nd 'forLoop': ");
+        for (int i = 0; i<= 10; i++){
+            System.out.println("This is 'i' number: " +i);
+            i++;
         }
-        System.out.println("Infinite For");
-        for (int i =0; i<=8; ++i){
-            i=7;
-            System.out.println("Number is: " + i);
-            if (i==7){
-                break;
-            }
+        System.out.println(" ");
+        System.out.println("This is 3-th 'forLoop': ");
+        for (int i = 0; i <= 10; i = i + 4){
+            System.out.println("This is 'i' number: " +i); // Print even values between 0 and 10
+        }
+        System.out.println(" ");
+        System.out.println("This is 4-th 'forLoop': ");
+        for (int i = 0; i <= 100; i = i + 15){
+            System.out.println("This is 'i' number: " +i);
         }
     }
     public static void nestedForLoop(){
-        for (int row=0; row<=10; row++){
-            System.out.println("This is row: " + row);
-            for (int column=0; column<=5; column++){
-                System.out.println("This is column: "+column);
+        // Outer loop
+        System.out.println(" ");
+        for (int i = 0; i <= 2; i++){
+            System.out.println("Outer loop: " + i);
+            // The "inner loop" will be executed one time for each iteration of the "Outer loop".
+            for (int j = 1; j <= 3; j++){
+                System.out.println("Inner loop: " +j);
+            }
+            for (int y = 1; y <=3; y++){
+                System.out.println("Second Inner loop: " + y);
+            }
+            for (int z = 1; z <=3; z++){
+                System.out.println("Third Inner loop: " + z);
             }
         }
     }
     public static void whileLoop(){
+        // We us it when we don’t know beforehand how many times is needed to repeat the instructions.
+        // The provided condition is checked before each repetition.
+            // As long as this condition is true, the instructions keep repeating.
         int num = 5;
-        while ((num < 10)){
-            //num++;
+        while (num < 10){
             System.out.println(num);
             num++;
         }
-    }
-    public static void doWhileLoop(){
-        int num = 5;
-        do {
-            num++;
-            System.out.println(num);
-            num++;
-        }while (num>10);
-    }
-    public static void forEachLoop(){
-        int[] num= {2,45,24,65};
-        for(int item: num){
-            System.out.println(item);
-            if (item != 24){
-                System.out.println("This is my number: " + item);
-            }
+        while (num < 15){
+                System.out.println(num);
+                num++;
         }
-
-        String[] names = {"Eli", "Vidko", "Misho", "Vasko","Planka", "Edi","Slav"};
-        for (String name: names){
-            System.out.println("My name is " + name);
-        }
-    }
-
-    public static void continueStatement(){
-        for (int i=7; i<10; i++){
-            System.out.println("Number is: "+ i);
-            System.out.println("This code is show always");
-            if (i>7){
-                System.out.println("Number is bigger than 7");
-                continue;
-            }
-            if (i<=10){
-                System.out.println("Number is less or = to 10");
-            }
-            System.out.println("Number is less or = to 8");
-        }
-        System.out.println("Next is WhileLoop");
-        int y = 5;
-        while (y<11){
-            y++;
-            System.out.println("Number is: "+ y);
-            System.out.println("This code is show always");
-            if (y<7){
-                System.out.println("Number is less to 7");
-                continue;
-            }
-            if (y>9){
-                System.out.println("Number is bigger than 9");
-                continue;
-            }
-            System.out.println("Number is = to 7 or 8 or 9");
-        }
-    }
-    public static void breakStatementWhileLoop(){
-        int y = 5;
-        while (y<11){
-            y++;
-            System.out.println("Number is: "+ y);
-            System.out.println("This code is show always");
-            if (y==7){
-                System.out.println("Number is == to 7");
-                break;
-            }
-            if (y<9){
-                System.out.println("Number is less than 9");
-            }
-        }
-        System.out.println("End code");
-    }
-    public static void breakStatementDoWhileLoop(){
-        int y = 5;
-        do{
-            System.out.println("Number is: "+ y);
-            System.out.println("This code is show always");
-            if (y==7){
-                System.out.println("Number is == to 7");
-                break;
-            }
-            if (y<9){
-                System.out.println("Number is less than 9");
-            }
-            y++;
-        }while (y<11);
-        System.out.println("End code");
     }
 }
+
