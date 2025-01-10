b8508d68d0b317e3d43af336f49c47f173b7efe5
Slav Lazarov <slavlzrv@gmail.com>
2025-01-10 07:55:09 +0200

    Add Lecture6

---
+++ src/Lecture6/Lecture6.java
@@ -0,0 +1,18 @@
+package Lecture6;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Lecture6 {
+    public static void main(String[] args) {
+        //arrayDemo();
+        //multidimensionalArrayDemo();
+        //arrayListDemo();
+        //outSideOfBoundErrorDemo();
+        //linkedListDemo();
+        //queueDemo();
+        // stackDemo();
+    }
+}
